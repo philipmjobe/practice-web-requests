0f84c0a2c7e095eb8a61399dffb6eaa5a668133e
Philip <philipmjobe@gmail.com>
2021-10-08 14:53:12 -0400

    added single definition

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -6,5 +6,5 @@ word = gets.chomp
 request = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=YOUR API KEY GOES HERE")
 
 request = request.parse(:json)
-
-puts request
+pp request
+puts request[0]["text"]
