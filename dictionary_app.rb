f44e5ed3f07e93ab45216d6b04081ec0bdef94e1
Philip <philipmjobe@gmail.com>
2021-10-08 14:59:15 -0400

    added system clear

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,9 +1,11 @@
 require "http"
 
+system "clear"
+
 puts "Give me a word that you would like a definiton for."
 word = gets.chomp
 
-request = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=YOUR API KEY GOES HERE")
+request = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
 
 request = request.parse(:json)
 pp request
