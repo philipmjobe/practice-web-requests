91d447ef2e17b09d8c81ad54a29e859547f2f6d6
Philip <philipmjobe@gmail.com>
2021-10-08 15:31:33 -0400

    added main definition

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -5,8 +5,13 @@ system "clear"
 puts "Give me a word that you would like a definiton for."
 word = gets.chomp
 
-request = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+request = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=fYOUR API KEY GOES HERE")
 
-request = request.parse(:json)
-pp request
-puts request[0]["text"]
+info = request.parse(:json)
+
+index = 0
+info.length.times do
+  info = info[index]
+  puts "#{index + 1}, #{info["text"]}"
+  index += 1
+end
